c8f8a47e6532da82c13299acab75d50deb17c6a7
sametsahin <sametsahinx@gmail.com>
2022-11-04 20:54:23 -0700

    begin of the mvc section

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
-import express from "express"
+import express from 'express'
+import dotenv from 'dotenv'
+import conn from './db.js'
 
+dotenv.config()
+
+//connection to the db
+conn()
 
 const app = express()
-const port = 3000
+const port = process.env.PORT
+
+//ejs template engine
+app.set('view engine', 'ejs')
+
+//static files
+app.use(express.static('public'))
+
 
 app.get("/", (req, res) => {
-    res.send('index page')
+    res.render('index')
+})
+app.get("/about", (req, res) => {
+    res.render('about')
 })
 
 app.listen(port, () => {
---
+++ db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const conn = () => {
+    mongoose.connect(process.env.DB_URI, {
+        dbName: 'nodeprj',
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(() => {
+        console.log('Connected to the DB')
+    }).catch((err) => {
+        console.log(`doesnt connected to db: ${err}`);
+    })
+}
+
+export default conn
